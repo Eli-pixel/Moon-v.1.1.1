317b5031f9912381ac16a955e4ea9a4c04335e26
Eli-pixel <62613272+Eli-pixel@users.noreply.github.com>
2020-04-23 11:33:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+###################
+#IMPORTS
+#################
+import time
+import datetime
+from tercolor import cprint as cpr
+
+
+
+################
+#STARTING
+##############
+
+
+moon = input("<-->")
+
+if moon == ("__get__ *FUNCTION_IMPORT* *moon*")
+  
