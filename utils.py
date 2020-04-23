e94c1332d97081c0e11435908724e792a6afe598
Eli-pixel <62613272+Eli-pixel@users.noreply.github.com>
2020-04-23 11:37:15 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,16 @@
+###################
+#IMPORTS
+##################
+import time
+import os
+
+
+
+####################
+#STARTING
+###################
+ 
+ 
+def CLEAR(clear):
+ os.system('cls' if os.name == 'nt' else 'clear')
+	
